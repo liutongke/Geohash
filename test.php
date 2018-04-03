20264c76ba82d41cf7745ea6d21556e6e29a6124
keke <liutongke@live.com>
2018-04-03 18:06:33 +0800

    增加了自定义推送

--- test.php
+++ test.php
@@ -7,6 +7,26 @@ $app_key = '***';
 $master_secret = '***';
 
 $jpush = new Jpush($app_key, $master_secret);
+$haha = $jpush->setPlatform('android')
+    //设置别名
+    ->addAlias('325')
+//{"msg_content":"点赞动态","extras":{
+//    "content":"阿哲0点赞了你的动态。"},"content_type":"text","title":"点赞动态!"}
+    //推送的消息体,安卓调用androidNotification，iOS调用iosNotification
+    ->Message(
+        [
+            //这里指定了，则会覆盖上级统一指定的 alert 信息；内容可以为空字符串，则表示不展示到通知栏。
+            "msg_content" => '山有木兮木有枝',
+            //这里自定义 JSON 格式的 Key/Value 信息，以供业务使用
+            "content_type" => 'text',
+            "title" => '测试',
+            'extras' => [
+                'content' => '心悦君兮君不知',
+                "badge" => (int)1,
+            ]
+        ])
+    ->send();
+var_dump($haha);
 
 $haha = $jpush->setPlatform('android')
     //设置别名
